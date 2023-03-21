03986ca5e3236bf5640ecb51d4080ce5072bd601
NamLe <phuongnam091097@gmail.com>
2023-03-21 07:52:08 +0700

    init commit

---
+++ app/src/main/java/com/example/architectureexample/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.architectureexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        NoteAdapter noteAdapter = new NoteAdapter();
+        recyclerView.setAdapter(noteAdapter);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // update RecycleView later
+                noteAdapter.setNotes(notes);
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/architectureexample/NoteDatabase.java
@@ -0,0 +1,54 @@
+package com.example.architectureexample;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance;
+
+    public abstract NoteDAO noteDAO();
+
+    public static synchronized NoteDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), NoteDatabase.class, "note_database")
+                    .fallbackToDestructiveMigration()
+                    // When instance is first created, it creates 3 notes
+                    .addCallback(roomCallback)
+                    .build();
+        }
+        return instance;
+    }
+
+    // For the first move, we want to make e.g. like 3 Notes in the db
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDAO noteDAO;
+
+        public PopulateDbAsyncTask(NoteDatabase noteDatabase) {
+            this.noteDAO = noteDatabase.noteDAO();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDAO.insert(new Note("Title 1", "Description 1", 1));
+            noteDAO.insert(new Note("Title 2", "Description 2", 2));
+            noteDAO.insert(new Note("Title 3", "Description 3", 3));
+            return null;
+        }
+    }
+}
